f9752b9694d5b4ec2a00b0857b533f1e4584a8e6
Dan Hensgen <dan@methodhead.com>
2015-09-25 22:47:17 -0400

    Extrace grid, add Grid#eachCell

---
+++ grid_test.js
@@ -0,0 +1,67 @@
+describe("Grid", function() {
+  describe("#new", function() {
+    it("should create a MxN grid of false values", function() {
+      var grid = new Grid(4, 4);
+      for (var x = 0; x < 4; x++) {
+        for (var y = 0; y < 4; y++) {
+          assert.equal(false, grid.cells[x][y], "at position (" + x + "," + y + ")" );
+        }
+      }
+    });
+  });
+
+  describe("#seedGrid", function() {
+    it("should seed the grid randomly per a random factor", function() {
+      var grid = new Grid(4, 4)
+      grid.seed();
+
+      var liveCount = 0;
+      for (var x = 0; x < 4; x++) {
+        for (var y = 0; y < 4; y++) {
+          if (grid.cells[x][y]) {
+            liveCount++;
+          }
+        }
+      }
+
+      assert(liveCount > 0, "there are some live cells");
+    });
+  });
+
+  describe("#addCells()", function() {
+    var grid;
+
+    before(function() {
+      grid = new Grid(4, 4)
+    });
+
+    it("should add cells to the grid", function() {
+      grid.addCells([[0,0], [2,3]])
+
+      assert.equal(true, grid.cells[0][0]);
+      assert.equal(true, grid.cells[2][3]);
+    });
+  });
+
+  describe("#eachCell()", function() {
+    it ("should iterate through each cell in the grid", function() {
+      var grid = new Grid(2, 2);
+      grid.addCells([[1,0]])
+
+      var callbacks = [];
+
+      grid.eachCell(function(x, y, state, thisArg) {
+        callbacks.push([x, y, state, thisArg]);
+      }, "thisArg");
+
+      assert.equal(4, callbacks.length);
+      assert.deepEqual([0, 0, false, "thisArg"], callbacks[0]);
+      assert.deepEqual([1, 0, true, "thisArg"], callbacks[1]);
+      assert.deepEqual([0, 1, false, "thisArg"], callbacks[2]);
+      assert.deepEqual([1, 1, false, "thisArg"], callbacks[3]);
+    });
+  });
+});
+
+
+
--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-Grid = function(m, n) {
-  this.cells = new Array(n);
-
-  for (var x = 0; x < n; x++) {
-    this.cells[x] = new Array(m);
-    for (var y = 0; y < m; y++) {
-      this.cells[x][y] = false;
-    }
-  }
-}
-
-Grid.prototype.seed = function() {
-  for (var y = 0; y < this.cells.length; y++) {
-    for (var x = 0; x < this.cells[y].length; x++) {
-      if (Math.random() > 0.5) {
-        this.cells[x][y] = true;
-      }
-    }
-  }
-}
-
-Grid.prototype.addCells = function(cells) {
-  cells.forEach(function addCell(cell) {
-    this.cells[cell[0]][cell[1]] = true;
-  }, this);
-}
-
-// procedural stuff //
-
+// done
 function createEmptyGrid(n, m) {
   var grid = new Array(n);
 
@@ -40,6 +12,7 @@ function createEmptyGrid(n, m) {
   return grid;
 }
 
+// done
 function seedGrid(grid) {
   for (var y = 0; y < grid.length; y++) {
     for (var x = 0; x < grid[y].length; x++) {
@@ -50,6 +23,7 @@ function seedGrid(grid) {
   }
 }
 
+// done
 function addToGrid(grid, points) {
   points.forEach(function(point) {
     grid[point[0]][point[1]] = true;
